e64f81b895cff056c335c590ac00c8da584e6cd4
Malatesh Gouda Hosamani <malateshgouda813@gmail.com>
2026-03-17 20:14:28 +0530

    Added .env and .gitignore

--- api/connect.php
+++ api/connect.php
@@ -1,9 +1,23 @@
 <?php
-$connect = mysqli_connect("localhost", "root", "", "voting") or die("Connection failed: ");
-if($connect){
-    
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Load .env file
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+// Database connection using env variables
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+$connect = mysqli_connect($host, $user, $password, $dbname);
+
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-else{
-    echo "Connection failed";
-}
+
 ?>
